387340ff546be91127e4b80d9e99979394b2c529
fabioftheodoro <fabio@ipcom.com.br>
2021-03-18 10:36:44 -0300

    Create trunk.sh

---
+++ trunk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+TRUNK1=$(sudo -u root /usr/sbin/asterisk -vvvr -rx "sip show peers" | grep UNREACHABLE | awk '{print$1}' | awk 'length($0) > 9')
+TRUNK2=$(sudo -u root /usr/sbin/asterisk -vvvr -rx "sip show peers" | grep UNKNOWN | awk '{print$1}' | awk 'length($0) > 9')
+
+
+if [ -n "$TRUNK1" ] || [ -n "$TRUNK2" ]
+then
+        echo "0"
+
+    else
+        echo "1"
+fi
+EOF
